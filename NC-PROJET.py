0deaac1c1522f32355bf3447b1eddfb65192c527
CThion <clement.thion@orange.fr>
2022-04-17 17:40:05 +0100

    Update NC-PROJET.py

--- NC-PROJET.py
+++ NC-PROJET.py
@@ -44,44 +44,51 @@ def ML_null1(V):
 def ML_null2(V):
     return gamma*(w_inf(V)-W/to_inf(V))
 
-def V_nullclineI(V, I):
+def V_nullcline(V):
     #return(-gCa*((1+tanh((V-V1)/V2))/2)*(V-ECa)-gL*(V-EL)+I)/gK*(V-EK)
     return (I - gL*(V-EL)-gCa*((1 + tanh((V -V1)/V2))/2)*(V-ECa))/(gK*(V-EK))
 
 
 def V_nullcline1(V):
-    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+10)/gK*(V-EK)
+    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+0)/(gK*(V-EK))
 def V_nullcline2(V):
-    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+20)/gK*(V-EK)
+    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+20)/(gK*(V-EK))
 def V_nullcline3(V):
-    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+60)/gK*(V-EK)
+    return(-gCa*(0.5*(1+tanh((V-V1)/V2)))*(V-ECa)-gL*(V-EL)+60)/(gK*(V-EK))
 
 
-V = linspace(-85,-75,100000)
-# y1=V_nullcline(V)
-# y2=w_inf(V)
-# y3 = V_nullcline1(V)
-# y4 = V_nullcline2(V)
-# y5 = V_nullcline3(V)
+V = linspace(-65,20,100000)
 
 
-# plt.plot(V, y1, 'r-')
-# plt.plot(V, y2, 'b-')
-# plt.plot(V, y3, 'y-')
-# plt.plot(V, y4, 'black')
-# plt.plot(V, y5, 'g-')
-
-# plt.xlabel('x')
-# plt.ylabel('y')
+# color=['r-', 'b-', 'y-', 'g-', 'p-']
+# for I in range(0,100,10):
+#     print(I)
+#     plt.plot(V, V_nullclineI(V, I), color[I%20])
 # plt.legend(['MN_null1', 'MN_null2'])
 # plt.title('Le temps passe et la mort approche')
 # plt.show()
 
-color=['r-', 'b-', 'y-', 'g-', 'p-']
-for I in range(0,100,10):
-    print(I)
-    plt.plot(V, V_nullclineI(V, I), color[I%10])
+
+y1=V_nullcline(V)
+y2=w_inf(V)
+y3 = V_nullcline1(V)
+y4 = V_nullcline2(V)
+y5 = V_nullcline3(V)
+
+
+plt.plot(V, y1, 'r-')
+plt.plot(V, y2, 'b-')
+plt.plot(V, y3, 'y-')
+plt.plot(V, y4, 'black')
+plt.plot(V, y5, 'g-')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend(['MN_null1', 'MN_null2'])
+plt.title('Le temps passe et la mort approche')
 plt.show()
+
+
     
 
 
